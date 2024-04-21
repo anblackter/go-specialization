d9e751a976fa59976c3855cfb4cc5ef1e8e5d1bc
anblackter <angel.henao@outlook.com>
2024-04-20 20:53:26 -0500

    feat(01-course findian): add findian file with the solution
    Write a program which prompts the user to enter a string. The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’. The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’, and contains the character ‘a’. The program should print “Not Found!” otherwise. The program should not be case-sensitive, so it does not matter if the characters are upper-case or lower-case.

---
+++ 01-course-getting-started/findian/findian.go
@@ -0,0 +1,26 @@
+// Write a program which prompts the user to enter a string. The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’. The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’, and contains the character ‘a’. The program should print “Not Found!” otherwise. The program should not be case-sensitive, so it does not matter if the characters are upper-case or lower-case.
+
+// Examples: The program should print “Found!” for the following example entered strings, “ian”, “Ian”, “iuiygaygn”, “I d skd a efju N”. The program should print “Not Found!” for the following strings, “ihhhhhn”, “ina”, “xian”. 
+
+// Submit your source code for the program,
+
+package main
+
+import ("fmt")
+
+func main(){
+	var input string
+	fmt.Print("Enter a string: ")
+	fmt.Scan(&input)
+	if input[0] == 'i' || input[0] == 'I' {
+		if input[len(input)-1] == 'n' || input[len(input)-1] == 'N' {
+			for i:=0; i<len(input); i++ {
+				if input[i] == 'a' || input[i] == 'A' {
+					fmt.Println("Found!")
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("Not Found!")
+}
